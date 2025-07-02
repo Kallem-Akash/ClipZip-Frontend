29d900c2cc487468b7c3ff640f563aae600be741
KALLEM AKASH <akashgoud018@gmail.com>
2025-07-02 14:38:39 +0530

    keys hidden

--- src/Logins/SupabaseClient.js
+++ src/Logins/SupabaseClient.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vtchxmolxwfpiejbabvg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0Y2h4bW9seHdmcGllamJhYnZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMDEzNDgsImV4cCI6MjA2NjY3NzM0OH0.bWFqrwSE5MCqlO9vIy3UXhkguCD2GWrTBEpal5pc9NM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

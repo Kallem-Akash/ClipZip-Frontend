774521b5b180003c6fe390a407ac6b6337d330a9
KALLEM AKASH <akashgoud018@gmail.com>
2025-07-02 20:32:52 +0530

    small error

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { supabase } from "../Logins/SupabaseClient"
+import { supabase } from "../Supabase/SupabaseClient"
 
 const LoginCard = () => {
   const [email, setEmail] = useState("")
